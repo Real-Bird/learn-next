5b3d0f339733ba844bd6640849144513990603e2
Real-Bird <marbil815@gmail.com>
2023-03-30 20:21:54 +0900

    fix: change ts->js & feat: api

---
+++ ssr-rest-api/pages/index.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export async function getServerSideProps() {
+  const usersReq = await axios.get(`${process.env.API_ENDPOINT}/api/04/users`);
+  return {
+    props: {
+      users: usersReq.data,
+    },
+  };
+}
+
+function List({ users }) {
+  console.log(users);
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link href={`/users/${user.username}`} passHref>
+            {user.username}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Users() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await fetch(`/api/04/users`);
+      const users = await req.json();
+
+      setLoading(false);
+      setData(users);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {loading && <div>Loading users...</div>}
+      {data && <List users={data} />}
+    </div>
+  );
+}
+
+export default Users;
---
+++ ssr-rest-api/pages/users/[username].js
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+  return {
+    props: {
+      username,
+      authorization: process.env.API_TOKEN,
+    },
+  };
+}
+
+function UserPage({ username, authorization }) {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await fetch(`/api/singleUser?username=${username}`);
+      const reqData = await req.json();
+
+      setLoading(false);
+      setData(reqData);
+    }
+
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <div>
+        <Link href="/" passHref>
+          Back to home
+        </Link>
+      </div>
+      <hr />
+      {loading && <div>Loading user data...</div>}
+      {data && <UserData user={data} />}
+    </div>
+  );
+}
+
+function UserData({ user }) {
+  return (
+    <div style={{ display: "flex" }}>
+      <img
+        src={user.profile_picture}
+        alt={user.username}
+        width={150}
+        height={150}
+      />
+      <div>
+        <div>
+          <b>Username:</b> {user.username}
+        </div>
+        <div>
+          <b>Full name:</b> {user.first_name} {user.last_name}
+        </div>
+        <div>
+          <b>Company:</b> {user.company}
+        </div>
+        <div>
+          <b>Job title:</b> {user.job_title}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserPage;
